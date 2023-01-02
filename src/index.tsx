8a126a5ff97cf85d639bd90aff7f69232737f621
seungtae <seungtae@bejewel.kr>
2023-01-03 08:31:53 +0900

    Feat : init project

---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+import './index.css'
+
+const router = createBrowserRouter([
+  { path: '/', element: <div>hello</div> },
+  {
+    path: '/auth',
+    element: <div>auth</div>
+  }
+])
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+)
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+)
