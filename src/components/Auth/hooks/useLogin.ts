45f991f42280d6a42763fd93e9f5b647e2f50dc8
seungtae <seungtae@bejewel.kr>
2023-01-07 00:06:05 +0900

    Feat : 토큰이 존재할경우 메인페이지로 바로 리다이렉트

--- src/components/Auth/hooks/useLogin.ts
+++ src/components/Auth/hooks/useLogin.ts
@@ -4,6 +4,7 @@ import { authApi } from '@components/Auth/api'
 import { AuthInfo, AuthSuccessResponse } from '@components/Auth/types'
 import { useMutation, UseMutateFunction } from '@tanstack/react-query'
 import { tokenKey } from '@components/Auth/constants'
+import { useEffect } from 'react'
 
 interface LoginHook {
   login: UseMutateFunction<AxiosResponse<AuthSuccessResponse>, unknown, AuthInfo>
@@ -16,5 +17,14 @@ export const useLogin = (): LoginHook => {
       navigate('/')
     }
   })
+
+  // 토큰이 있을 경우 메인 페이지로 리다이렉트
+  useEffect(() => {
+    const token = sessionStorage.getItem(tokenKey)
+    if (token !== null) {
+      navigate('/')
+    }
+  }, [])
+
   return { login: mutate }
 }
