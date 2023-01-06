aa34bb3acc722bae95cf0ea5ac77e65cc84db370
seungtae <seungtae@bejewel.kr>
2023-01-06 12:11:33 +0900

    Feat : 정상적인 입력일때만 버튼 활성화

--- src/components/Auth/AuthForm/AuthForm.tsx
+++ src/components/Auth/AuthForm/AuthForm.tsx
@@ -15,7 +15,7 @@ const AuthForm: FC<AuthFormProps> = ({ onSubmit }) => {
         <form onSubmit={handleSubmit(onSubmit)}>
             <Input label='이메일' errorMessage={formState.errors.email?.message} {...register('email', { required: true, pattern: { value: emailRegExp, message: '이메일 형식이 올바르지 않습니다' } })}/>
             <Input label='비밀번호' errorMessage={formState.errors.password?.message} {...register('password', { required: true, minLength: { value: 8, message: '비밀번호는 최소 8자 이상입니다' } })}/>
-            <button>회원가입</button>
+            <button disabled={!formState.isValid}>회원가입</button>
         </form>
   )
 }
